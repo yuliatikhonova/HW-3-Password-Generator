4e5670014b796c34fc9bc28cbe69f6eb72d6e742
yulia tikhonova <yuliatikhonova@outlook.com>
2020-04-18 19:22:07 -0700

    set up the length prompt and linked it to html. Also set up the four character checkboxs and linked it to html

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-  // Variable for the button linked to html
+// Variable for the button linked to html
 var generateBtn = document.querySelector("#generate");
 
-  // Write password to the #password input
+// Write password to the #password input
 function writePassword() {
   console.log();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = "password";
 }
 
-  // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //---------------------------------------------------------------------------
 
-  // function for generating the password
+// function for generating the password
 function generatePassword() {
-  var criteria = userCriteria();
+  var criteria = userCriteria(length, character);
   var newPassword = criteriaToPassword(criteria);
   return newPassword;
 }
+//Prompt to get length
+function lengthPrompt() {
+  var minLengthAnswer = prompt("Please enter your minimum length");
+  var maxLengthAnswer = prompt("Please enter your maximum length");
+  if (minLengthAnswer != null) {
+    document.getElementById("demo").innerHTML =
+      " min LENGTH IS " + minLengthAnswer +
+      " max LENGTH IS " + maxLengthAnswer;
 
-  // (generatePassword function) is calling out this function
-function userCriteria(length, lowerCase, upperCase, numeric, special) {
+
+    //setLength(lengthAnswer);
+  }
+
+}
+
+//Prompt to get characters
+function characterPrompt() {
+  var isLowerRadio = document.getElementById("lowerRadio");
+  var isUpperRadio = document.getElementById("upperRadio");
+  var isNumericRadio = document.getElementById("numericRadio");
+  var isSpecialRadio = document.getElementById("specialRadio");
+}
+
+
+
+// function for setting user criteria
+function userCriteria(lengthInput) {
   var criteriaObject = {
-    types: {
+    length: 0,
+    character: {
       lowercase: false,
       uppercase: false,
       numeric: false,
       special: false
     }
   }
+
+  // setLength: function(lengthInput) {
+  //   this.length = lengthInput;
+  // }
+
+
+  return criteriaObject
 }
+
+
 
 //
 function criteriaToPassword(criteria) {
+
 
 }
